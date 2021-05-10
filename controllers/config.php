9d44b91c419e35f6d059a283c9629ae079fe7591
Nathan7471 <54980608+mrnat7471@users.noreply.github.com>
2021-05-10 01:23:19 +0100

    Update config.php

--- controllers/config.php
+++ controllers/config.php
@@ -1,8 +1,8 @@
 <?php
-define('DB_SERVER', 'localhost:3306'); #localhost:3306
-define('DB_USERNAME', 'Nathan'); #Nathan
-define('DB_PASSWORD', 'b6_08yDb'); #b6_08yDb
-define('DB_NAME', 'recruitment');
+define('DB_SERVER', '#');
+define('DB_USERNAME', '#');
+define('DB_PASSWORD', '#');
+define('DB_NAME', '#');
 
 $servername = DB_SERVER;
 $username = DB_USERNAME;
